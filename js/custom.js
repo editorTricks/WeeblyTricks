d4f0aca96fd0da99a8b13420bc8b2094c735e670
Chris <weeblytricks@gmail.com>
2020-02-12 10:10:15 -0500

    Header Update

--- js/custom.js
+++ js/custom.js
@@ -4,16 +4,15 @@ jQuery(function($) {
   var header = $("<div>").attr("id", "header");
   
   header.html([
-    "<div class'header'>",
-	  "<div class='container'>",
+	"<div class='container'>",
 		"<div class='logo'><a href='/index.html'><img src='images/wt_logo.png' /></a></div>",
-		"<div class='navigation'>",
-			"<ul>",
-				"<li><a href='/index.html'>Home</a></li>",
-				"<li>About</li>",
-				"<li>Contact</li>",
-			"</ul>",
-		"</div>",
+			"<div class='navigation'>",
+				"<ul>",
+					"<li><a href='/index.html'>Home</a></li>",
+					"<li>About</li>",
+					"<li>Contact</li>",
+				"</ul>",
+			"</div>",
 	  "</div>",
 	"</div>"
   ]);
