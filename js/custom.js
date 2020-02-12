b3fe2aecea39b874e360c2af57212b00aec9ee86
Chris <weeblytricks@gmail.com>
2020-02-12 10:41:58 -0500

    Header Update

--- js/custom.js
+++ js/custom.js
@@ -2,9 +2,10 @@ jQuery(function($) {
   
   // Global HTML Elements
   var header = $("<div>").attr("class", "header");
+  var banner = $("<div>").attr("class", "banner");
   
   header.html([
-	"<div class='container second'>",
+	"<div class='container'>",
 		"<div class='logo'>",
 		    "<a href='/index.html'><img src='images/wt_logo.png' /></a></div>",
 			"<div class='navigation'>",
@@ -18,6 +19,15 @@ jQuery(function($) {
 	"</div>"
   ].join(""));
   
-  $("body").prepend(header);
+  banner.html([
+	"<div class='banner'>",
+		"<div class='container'>",
+			"<h2>Tutorial for your Weebly Site</h2>",
+			"<p>Let's level up your Weebly site!</p>",
+		"</div>",
+	"</div>"
+  ].join(""));
+  
+  $("body").prepend(header banner);
   
 });
